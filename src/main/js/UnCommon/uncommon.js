06cf509f0d5658495ca45bea4e4f11eec3c411fb
kushpandya <kushpandya@vasyerp.com>
2024-10-08 14:52:43 +0530

    test

--- src/main/js/UnCommon/uncommon.js
+++ src/main/js/UnCommon/uncommon.js
@@ -10,7 +10,7 @@
     };*/
     var save_flag=0;
 $(document).ready(function () {
-	 var bankForm = $('#unCommon_form_2');
+	 var bankForm = $('#unCommon_form_3');
        bankForm.formValidation({
         framework: 'bootstrap',
         excluded: ":disabled",
