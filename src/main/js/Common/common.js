e9bee47695d60ee8c68f3300ddf1ea4e97d838a5
kushpandya <kushpandya@vasyerp.com>
2024-10-09 11:53:56 +0530

    commonJsUpdate

--- src/main/js/Common/common.js
+++ src/main/js/Common/common.js
@@ -10,7 +10,7 @@
     };*/
     var save_flag=0;
 $(document).ready(function () {
-	 var bankForm = $('#bank_form10');
+	 var bankForm = $('#bank_form11');
        bankForm.formValidation({
         framework: 'bootstrap',
         excluded: ":disabled",
